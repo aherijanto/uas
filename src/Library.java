a66b683c4b8eb5272229d80371abab1e5f031041
bistique <aherijanto@gmail.com>
2021-01-08 21:43:36 +0700

    Ujian Akhir Semester Kelas 1C, 1D, 1E Politeknik Harapan Bersama DIV Teknik Informatika Semester 1 2020/2021 (Januari 8,2021)

--- src/Library.java
+++ src/Library.java
@@ -1,3 +1,5 @@
+import org.jetbrains.annotations.NotNull;
+
 public class Library {
     public static void PrintTitle(String Judul){
         System.out.println(Judul);
@@ -5,7 +7,7 @@ public class Library {
     }
 
 
-    public static void HitungTotalAngka(int[] isiAngka) {
+    public static void HitungTotalAngka(int @NotNull [] isiAngka) {
         double jumlahTotalAngka = 0;
         for(int index = 0; index < isiAngka.length; index++){
             System.out.println("Angka Dalam Array Index Ke " + index + " Adalah " + isiAngka[index]);
